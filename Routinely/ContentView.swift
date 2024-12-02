c74aa47330523238ae4377d2d7bfb03391b5a2fc
Mark H <mark@austincloud.guru>
2024-12-02 09:51:36 -0600

    Initial Commit

---
+++ Routinely/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Routinely
+//
+//  Created by Mark Honomichl on 12/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Routinely/RoutinelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutinelyApp.swift
+//  Routinely
+//
+//  Created by Mark Honomichl on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct RoutinelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
